120dfcabf72da80d9e3f478a1374086296e8c610
youhyojin <73086022+youhyojin1@users.noreply.github.com>
2022-08-09 01:15:37 +0900

    Create Framework

---
+++ A Module/A_Module.h
@@ -0,0 +1,18 @@
+//
+//  A_Module.h
+//  A Module
+//
+//  Created by ZUMIN YOU on 2022/08/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for A_Module.
+FOUNDATION_EXPORT double A_ModuleVersionNumber;
+
+//! Project version string for A_Module.
+FOUNDATION_EXPORT const unsigned char A_ModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <A_Module/PublicHeader.h>
+
+
